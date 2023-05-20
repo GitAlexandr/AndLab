6efaaec31cccb61772e799d76d7cfd17932790fd
GitAlexandr <sashabushuev03@mail.ru>
2023-05-20 11:11:30 +0300

    add_new_GIFs

--- pleasework/app/src/main/java/com/example/pleasework/MainActivity.kt
+++ pleasework/app/src/main/java/com/example/pleasework/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -16,20 +17,24 @@ const val BASE_URL = "https://api.giphy.com/v1/"
 const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: GifsAdapter
+    private lateinit var gifs: MutableList<DataObject>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        val gifs = mutableListOf<DataObject>()
-        val adapter = GifsAdapter(this, gifs)
+        gifs = mutableListOf()
+        adapter = GifsAdapter(this, gifs)
 
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = GridLayoutManager(this, 2)
 
-        adapter.setOnItemClickListener(object : GifsAdapter.OnItemClickListener{
+        adapter.setOnItemClickListener(object : GifsAdapter.OnItemClickListener {
             override fun onItemClick(position: Int) {
                 val intent = Intent(this@MainActivity, SecondActivity::class.java)
                 intent.putExtra("url", gifs[position].images?.ogImage?.url)
@@ -37,21 +42,37 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        fetchGIFs()
+    }
+
+    fun onUpdateButtonClick(view: View) {
+        fetchGIFs()
+    }
+
+    private fun fetchGIFs() {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val retroService = retrofit.create(DataService::class.java)
-        retroService.getGifs().enqueue(object : Callback<DataResult?>{
+        retroService.getGifs().enqueue(object : Callback<DataResult?> {
             override fun onResponse(call: Call<DataResult?>, response: Response<DataResult?>) {
                 val body = response.body()
-                if(body==null){
+                if (body == null) {
                     Log.d(TAG, "onResponse: No response...")
                     return
                 }
 
-                gifs.addAll(body.res)
+                val newGifs = body.res
+                
+                val uniqueGifs = newGifs.filterNot { newGif ->
+                    gifs.any { existingGif ->
+                        existingGif.images?.ogImage?.url == newGif.images?.ogImage?.url
+                    }
+                }
+
+                gifs.addAll(uniqueGifs)
                 adapter.notifyDataSetChanged()
             }
 
